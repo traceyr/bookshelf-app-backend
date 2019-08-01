0ecf5531384509e7b67db41cb37369148908869f
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2019-07-31 17:17:04 -0700

    basic setup and got ndoe to send data to react

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('datatdslkfjdlskjkl');
+});
+
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
